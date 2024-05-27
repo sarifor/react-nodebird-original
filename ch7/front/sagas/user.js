22029037654a7505d4a8c4a86173992e6e2f9a17
sarifor <iread1q84@gmail.com>
2024-05-27 15:49:35 +0900

    Fix: Modify comment

--- ch7/front/sagas/user.js
+++ ch7/front/sagas/user.js
@@ -189,7 +189,7 @@ function* logOut() {
 }
 
 function signUpAPI(data) {
-  return axios.post('/user', data); // http://api.nodebird.com/user
+  return axios.post('/user', data); // 개발 환경: http://localhost:3065/user, 프로덕션 환경: http://api.nodebird.com/user
 }
 
 function* signUp(action) {
