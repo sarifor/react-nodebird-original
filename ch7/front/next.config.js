f5d5f293244834da39b93ba4580950b0cf6bf173
sarifor <iread1q84@gmail.com>
2024-05-27 11:51:56 +0900

    Update: Ignore Eslint rules to build

--- ch7/front/next.config.js
+++ ch7/front/next.config.js
@@ -4,6 +4,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   compress: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack(config, { webpack }) {
     const prod = process.env.NODE_ENV === 'production';
     return {
